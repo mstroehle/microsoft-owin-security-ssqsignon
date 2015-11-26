cd71939f0b1113eb49648e2329a683ef7d5119ec
Piotr Wójcik <piotr.wojcik@rivierasolutions.pl>
2015-11-27 00:08:03 +0100

    Added dedicated Filter and Attribute.

---
+++ SSQSignonAuthenticationAttribute.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace Microsoft.Owin.Security.SSQSignon
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class SSQSignonAuthenticationAttribute : Attribute, IAuthenticationFilter
+    {
+        private readonly IAuthenticationFilter _innerFilter;
+
+        public SSQSignonAuthenticationAttribute()
+            : this(new SSQSignonAthenticationFilter())
+        {
+        }
+
+        internal SSQSignonAuthenticationAttribute(IAuthenticationFilter innerFilter)
+        {
+            if (innerFilter == null)
+            {
+                throw new ArgumentNullException("innerFilter");
+            }
+
+            _innerFilter = innerFilter;
+        }
+
+        public bool AllowMultiple
+        {
+            get { return true; }
+        }
+
+        public string AuthenticationType
+        {
+            get { return SSQSignonDefaults.AuthenticationType; }
+        }
+
+        internal IAuthenticationFilter InnerFilter
+        {
+            get
+            {
+                return _innerFilter;
+            }
+        }
+
+        /// <inheritdoc />
+        public Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        {
+            return _innerFilter.AuthenticateAsync(context, cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            return _innerFilter.ChallengeAsync(context, cancellationToken);
+        }
+    }
+}
